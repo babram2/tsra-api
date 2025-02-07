d3634ab3a802775732619b597ce502f661599c9b
babram2 <atmstardust@gmail.com>
2025-02-07 01:17:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import stripe
 import datetime
 from geopy.geocoders import Nominatim
 from geopy.distance import geodesic
+from werkzeug.urls import url_parse
 
 # Initialisation de Flask et des extensions
 app = Flask(__name__)
